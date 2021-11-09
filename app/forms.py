98f37d05160b2e364546685cca0d4b398d184b19
Amirhossein Sohrabbeig <amirhossein@Amirhosseins-MacBook-Pro.local>
2021-11-09 11:34:26 -0700

    Edit profile is added.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+     TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
+    Length
 from app.models import User
 
 
@@ -26,3 +28,13 @@ class RegisterationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError("Try a different email address.")
+
+class EditProfileForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    about_me = TextAreaField("About_me", validators=[Length(min=0, max=140)])
+    submit = SubmitField("Submit")
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("This username has already been taken. Try another one.")
--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,22 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegisterationForm
+from app.forms import EditProfileForm, LoginForm, RegisterationForm
 from app.models import User
+from datetime import datetime
 
-from app.models import User
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
 
 @app.route("/")
 @app.route("/index")
 @login_required
 def index():
-    return render_template("index.html", title="Home")
+    return render_template("index.html", title="Home", user=current_user)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -29,7 +35,7 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template("login.html", title="Login", form=form)
+    return render_template("login.html", title="Login", form=form, user=current_user)
 
 @app.route('/logout')
 def logout():
@@ -49,4 +55,25 @@ def register():
         flash("Congratulations! your are now registered.")
         return redirect(url_for('login'))
     
-    return render_template("register.html", title="Register", form=form)
+    return render_template("register.html", title="Register", form=form, user=current_user)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', title='profile', user=user)
+
+@app.route('/edit_profile', methods = ["GET", "POST"])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash("Your changes have been saved!")
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template("edit_profile.html", title= "Edit Profile", user=current_user, form=form)
