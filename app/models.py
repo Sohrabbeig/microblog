c3822d282789b4148edd25d0f896747b814c5859
Amirhossein Sohrabbeig <amirhossein@Amirhosseins-MacBook-Pro.local>
2021-11-13 12:06:26 -0700

    followers model implemented

--- app/models.py
+++ app/models.py
@@ -1,8 +1,16 @@
 from datetime import datetime
+
+from sqlalchemy.orm import backref
 from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
+
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 
 class User(db.Model, UserMixin):
@@ -10,9 +18,17 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    posts = db.relationship('Post', backref='author', lazy='dynamic', cascade='all, delete')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin = (followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy = 'dynamic'
+    )
 
     def __repr__(self):
         return "<User {}>".format(self.username)
